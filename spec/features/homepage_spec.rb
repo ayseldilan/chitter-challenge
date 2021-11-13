1da04b06a3d35eb71cef204dc6fb4ca7fe8ae5d6
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2021-11-13 15:41:24 +0000

    passed feature test

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,6 @@
+feature 'Creating Chitter home page' do
+    scenario 'User can view the home page' do
+        visit('/')
+        expect(page).to have_content 'Chitter page working!'
+    end
+end
